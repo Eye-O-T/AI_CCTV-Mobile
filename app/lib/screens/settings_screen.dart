d28a643df4fe65f9ece06e1fefaf8c990917f9ea
jdh4568 <jdh4568@naver.com>
2026-08-05 11:53:02 +0900

    기본 화면

---
+++ app/lib/screens/settings_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LiveScreen extends StatelessWidget {
+  const LiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('설정 화면'),
+    );
+  }
+}
