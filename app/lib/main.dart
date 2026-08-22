3ee7fee2985a46cebdb2ebdf6ba9e018e1f9da08
jdh4568 <jdh4568@naver.com>
2026-08-22 16:31:34 +0900

    플러터 앱 FCM연결 - 현재 FCM 다이렉트 메시지 수신 확인

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'router/app_router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  await setupFcm();
+
   runApp(
     const ProviderScope(
       child: MyApp(),
     ),
   );
+}
+
+Future<void> setupFcm() async {
+  final messaging = FirebaseMessaging.instance;
+
+  final settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  debugPrint(
+    '알림 권한: ${settings.authorizationStatus}',
+  );
+
+  final token = await messaging.getToken();
+
+  debugPrint('FCM Token: $token');
 }
 
 class MyApp extends StatelessWidget {
